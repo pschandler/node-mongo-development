8145dc9ec249424b0ca0c3e8e5a731f8ac93f6a1
Patrick Schandler <pschandler@gmail.com>
2024-04-04 09:09:55 -0500

    test full funtionality

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  console.log("index: ");
-  res.render("index");
-  // Task.find()
-  //   .then((tasks) => {
-  //     const currentTasks = tasks.filter(task => !task.completed);
-  //     const completedTasks = tasks.filter(task => task.completed === true);
-
-  //     console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-  //     res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.send('Sorry! Something went wrong.');
-  //   });
+  // console.log("index: ");
+  // res.render("index");
+  Task.find()
+    .then((tasks) => {
+      const currentTasks = tasks.filter((task) => !task.completed);
+      const completedTasks = tasks.filter((task) => task.completed === true);
+      console.log(
+        `Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`
+      );
+      res.render("index", {
+        currentTasks: currentTasks,
+        completedTasks: completedTasks,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("Sorry! Something went wrong.");
+    });
 });
 
 router.post("/addTask", function (req, res, next) {
@@ -65,7 +69,7 @@ router.post("/completeTask", function (req, res, next) {
 
 router.post("/deleteTask", function (req, res, next) {
   const taskId = req.body._id;
-  const completedDate = Date.now();
+  const completedDate = "2024-03-30T14:51:25.815+00:00"; //Date.now();
   Task.findByIdAndDelete(taskId)
     .then(() => {
       console.log(`Deleted task $(taskId)`);
